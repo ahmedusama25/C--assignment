b87acfb5fa75736e56378868b2153ec1c60ff3d3
ahmedusama25 <57043086+ahmedusama25@users.noreply.github.com>
2020-07-29 13:52:50 +0500

    practice projects

---
+++ reversing order of word in sentence/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence");
+            string sentence = Console.ReadLine();
+            string[] a = sentence.Split(" ");
+            for (int i = 0; i<a.Length; i++){
+                Console.Write(a[a.Length-1-i] + " ");
+            }
+            
+        }
+    }
+}
